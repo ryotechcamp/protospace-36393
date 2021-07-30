edc156687ad8f4d36d4117d44e0eb5d982f6f729
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-07-30 13:36:20 +0900

    他ユーザーによる編集防止
    ログイン状態のユーザーであっても、他のユーザーのプロトタイプ編集ページのURLを直接入力して遷移しようとすると、トップページにリダイレクトされるよう修正しました

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :new, :show,]
-  before_action :move_to_index, only: :edit
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -26,6 +25,10 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+
+    unless @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
   def update
@@ -46,12 +49,6 @@ class PrototypesController < ApplicationController
   private 
   def prototype_params
    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
   end
 
   end
